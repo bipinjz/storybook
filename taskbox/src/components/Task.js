8b8f8a59cb79412b34915eb2101541a706329db9
bipinjz <bipinjz@gmail.com>
2022-04-09 16:59:47 +1000

    update

--- taskbox/src/components/Task.js
+++ taskbox/src/components/Task.js
@@ -20,7 +20,7 @@ export default function Task({task: { id, title, state }, onArchiveTask, onPinTa
                 />
             </label>
             <div className="title">
-                <input type="text" value={title} readOnly={true} placeholder='Input title' style={{ background: 'red' }} />
+                <input type="text" value={title} readOnly={true} placeholder='Input title' style={{ background: 'yellow' }} />
             </div>
             
             <div className="actions" onClick={event => event.stopPropagation()}>
